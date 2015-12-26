342f0004dc81385344874a7ad37e1502dc28ba76
mnvini <viniciusrcr@hotmail.com>
2015-12-26 00:26:55 -0200

    product class

---
+++ Vinicius.VirtualStore.Domain/Entities/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vinicius.VirtualStore.Domain.Entities
+{
+    public class Products
+    {
+        public int ProductId{ get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+    }
+}
