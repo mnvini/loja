202a4e61b3330ac5afe183faed03f1927adf4c82
mnvini <viniciusrcr@hotmail.com>
2016-01-01 00:11:24 -0200

    happy new year

--- Vinicius.VirtualStore.Domain/Entities/Cart.cs
+++ Vinicius.VirtualStore.Domain/Entities/Cart.cs
@@ -1,18 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Vinicius.VirtualStore.Domain.Entities
 {
     public class Cart
     {
+        private readonly List<CartItems> _cartitems = new List<CartItems>(); 
+
+        public void AddItem(Products products, int quantity)
+        {
+            CartItems cart = _cartitems.FirstOrDefault(p => p.Procuts.ProductId == products.ProductId);
+
+            if (cart == null)
+            {
+                _cartitems.Add(new CartItems
+                {
+                    Procuts = products,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                cart.Quantity += quantity;
+            }
+        }
+
+        public void RemoveItem(Products products)
+        {
+            _cartitems.RemoveAll(l => l.Procuts.ProductId == products.ProductId);
+        }
+
+        public decimal TotalValue()
+        {
+           return _cartitems.Sum(p => p.Procuts.Price*p.Quantity);
+        }
+
+        public void CleanCart()
+        {
+            _cartitems.Clear();
+        }
+
+        public IEnumerable<CartItems> CartItems
+        {
+            get { return _cartitems; }
+        } 
     }
 
 
     public class CartItems
     {
-        
+        public Products Procuts { get; set; }
+
+        public int Quantity { get; set; }
     }
 }
---
+++ Vinicius.VirtualStore.Domain/Repository/EfDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vinicius.VirtualStore.Domain.Entities;
+
+namespace Vinicius.VirtualStore.Domain.Repository
+{
+    public class EfDbContext: DbContext
+    { 
+        public DbSet<Products> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Products>().ToTable("Produtos");
+        }
+    }
+}
